4106075fe570bcbe75a88ce0ca7d6c7a3c53024f
pratik5045 <162993462+pratik5045@users.noreply.github.com>
2024-07-31 09:20:16 -0700

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React,{ useRef, useState } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import './App.css'
+import Nav from './components/Nav';
+import {Navigation} from 'swiper/modules';
+// import { MdWifiCalling } from "react-icons/md";
+
+ export default function App() {
+  // let Navitems = ['Explore','LUXURY GATEWAYS','LIST YOUR PROPERTY','']
+return <>
+<Nav></Nav>
+</>
+
+
+ }
+
